5eafc1b03acb3eba9c0e145ec56a03cabf2a1cfd
marcoaureliosf <aurelius.sf@gmail.com>
2022-03-23 16:05:52 -0300

    chore: config default

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+export function Home() {
+  return (
+    <>
+      <Head>
+        <title>ig.news</title>
+      </Head>
+
+      <h1>Hello World!</h1>
+    </>
+  )
+}
+
+export default Home
