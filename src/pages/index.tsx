451ef21e8c57c83cf866df92fb2fab9ad172e836
marcoaureliosf <aurelius.sf@gmail.com>
2022-03-24 01:13:10 -0300

    feat: components and pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,31 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head'
+import Image from 'next/image'
 
-export function Home() {
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import styles from './home.module.scss'
+
+function Home() {
   return (
     <>
       <Head>
-        <title>ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>Hello World!</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get acess to all the publications <br />
+            <span>for $9.90 mounth</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <Image src="/images/avatar.svg" alt="Girl coding" width={336} height={521} />
+      </main>
     </>
   )
 }
